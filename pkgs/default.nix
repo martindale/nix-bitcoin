7e6e6993f7bf7cdc1f305704af70dcec1f6131c5
Jonas Nick <jonasd.nick@gmail.com>
2020-02-19 16:35:42 +0000

    Add nixpkgs-pinned and nixops19_09 to default pkgs.
    It's helpful to be able to use these packages when importing nix-bitcoin.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,4 +10,6 @@
   pylightning = pkgs.python3Packages.callPackage ./pylightning { };
   liquid-swap = pkgs.python3Packages.callPackage ./liquid-swap { };
   generate-secrets = pkgs.callPackage ./generate-secrets { };
+  nixpkgs-pinned = ./nixpkgs-pinned.nix;
+  nixops19_09 = pkgs.callPackage ./nixops { };
 }
